008ad9b8484e0f16af3c96cb462aec0529775bfb
Tilak <tilakacharya903@gmail.com>
2023-01-20 17:42:59 -0500

    Initial Commit

---
+++ A1_A2_iOS_Tilak_c0868747/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_Tilak_c0868747
+//
+//  Created by Tilak Acharya on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
